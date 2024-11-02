f672198991678e5a803a66cdb5c9b94b6c9387ef
Akshat2920 <81002264+Akshat2920@users.noreply.github.com>
2024-11-02 08:04:33 +0530

    Delete repo directory

--- repo/CustomerRepo.java
+++
@@ -1,7 +0,0 @@
-package org.akshat.yummy.repo;
-
-import org.akshat.yummy.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepo extends JpaRepository<Customer, Long> {
-}
